2a3f7c8c228d074095375f69b9ddd4e3c2a4c536
Adrian-F <adrianfeloh1@gmail.com>
2022-07-23 11:31:14 -0500

    5-Sección main HTML y parte del CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,38 @@
-const { series, src, dest, watch} = require('gulp');
+const { series, src, dest, watch, parallel} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require("gulp-imagemin");
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
+
 
 function css( ) {
-    return src ("src/scss/app.scss")
+    return src ("src/scss/**/*.scss")
         .pipe( sass() )
-        .pipe( dest("./build/css") )
+        .pipe( dest("./build/css") );
 }
 
 function minificarCss( ) {
-    return src ("src/scss/app.scss")
+    return src ("src/scss/**/*.scss")
         .pipe (sass({
             outputStyle: "expanded"
         }))
-        .pipe (dest ("./build/css"))
+        .pipe (dest("./build/css"));
 }
+
+function imagenes() {
+    return src("src/img/**/*")
+    .pipe(imagemin())
+    .pipe(dest("./build/img"))
+    .pipe(notify({ message: "Imagen Minificada"}));
+  }
+
+function versionWebp(){
+    return src("src/img/**/*")
+    .pipe (webp())
+    .pipe (dest("./build/img"))
+    .pipe (notify({ message: "Imagen webp"}))
+}
+  
 
 function watchArchivos() {
     watch ("src/scss/**/*.scss", css); // * es igual a la carpeta actual
@@ -22,4 +41,9 @@ function watchArchivos() {
 
 exports.css = css;
 exports.minificarCss = minificarCss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+
+exports.default = series ( css, imagenes, versionWebp, watchArchivos)
+
+/* SE LLAMA EN LA TERMINAL SOLO CON LA PALABRA GULP */
