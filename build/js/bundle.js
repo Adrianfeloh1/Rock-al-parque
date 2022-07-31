0890e7fb30ba0cfa479f37224ab7db4708d43093
Adrian-F <adrianfeloh1@gmail.com>
2022-07-31 11:01:42 -0500

    7-Funcionalidad a la galería con javascript y a parte del body con overlay

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -11,10 +11,17 @@ function crearGaleria (){
 
         //Primero se crea la imagen
         const imagen = document.createElement ("IMG");
-        imagen.src = `build/img/thumb/${i}.webp`;    
+        imagen.src = `build/img/thumb/${i}.webp`;
+
+        //3. nuevo atributo de html 5 para crear variables hacia el html
+        imagen.dataset.imagenId = i;    
 
         //para verificar en la consola
             //console.log(imagen);
+
+        /* 2. SE AÑADE LA FUNCION DE MOSTRAR IMAGEN */
+        //se crea con el evento onclick
+        imagen.onclick = mostrarImagen
 
         // Despues agrego el li
         const lista = document.createElement ("LI");
@@ -25,4 +32,45 @@ function crearGaleria (){
         //por ultimo agregamos los li a la galeria
         galeria.appendChild(lista);
     }
+}
+
+/* 2. SEGUNDA FUNCION PARA MOSTRAR UNA IMAGEN SELECCIONADA MAS GRANDE */
+
+function mostrarImagen(e) {
+    //se debe pasar de string a number    
+    const id = parseInt(e.target.dataset.imagenId)// 3. dataset
+    
+    const imagen = document.createElement ("IMG");
+        imagen.src = `build/img/grande/${id}.webp`;
+        // console.log(imagen); para verificar que tome la imagen grande
+
+        //PARA HACER LA SOMBRA DE FONDO CUANDO SE AMPLIA LA IMAGEN
+        const overlay = document.createElement ("DIV");//se crea el div
+        overlay.appendChild(imagen) // y se agrega la imagen
+        overlay.classList.add("overlay")//se agrega una clase en css
+
+        //LA IMAGEN QUEDA ABIERTA, ENTONCES DEBEMOS CREAR UN BOTON
+        const cerrarImagen = document.createElement ("P");
+        cerrarImagen.textContent = "X"; 
+        cerrarImagen.classList.add ("boton-cerrar"); // se crea una clase en css
+
+        //Cuando se presiona cierra la imagen
+        cerrarImagen.onclick = function () {
+            overlay.remove(); // con esto cerramos la imagen al darle click al la X 
+        }
+
+        //Cuando se presiona fuera de la imagen (en el overlay)
+        overlay.onclick = function () {
+            overlay.remove(); // con esto cerramos la imagen al darle click al la X 
+        }
+
+        overlay.appendChild(cerrarImagen);
+
+        //PARA MOSTRARLO EN EL BODY DEL HTML
+        const body = document.querySelector("body");
+        body.appendChild(overlay) // con esto se van creando divs en el html con cada click
+
+        //PARA FIJAR LA IMAGEN SIN EL SCROLL
+        body.classList.add("fijar-body") //se agrega css preferiblemente en los blobales
+
 }
