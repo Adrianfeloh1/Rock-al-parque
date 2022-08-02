c292b16fc846e5f956a805b35174b33a317c1122
Adrian-F <adrianfeloh1@gmail.com>
2022-08-02 18:32:39 -0500

    8-Finalizando la sección boletas, footer y funcionalidades con JS a la navbar

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,5 +1,25 @@
+document.addEventListener("DOMContentLoaded", function (){
+    scrollNav()
+})
 
+function scrollNav() {
+    const enlaces = document.querySelectorAll (".navegacion-principal a");
+    //console.log(enlaces); //para verificar que aparezcan todos los enlaces
 
+    enlaces.forEach(function(enlace){ //Para iterar cada uno de los elementos
+        //console.log(enlace); // Verificamos que ya accedemos a cada enlace
+        enlace.addEventListener("click", function(e){
+            e.preventDefault();
+            //console.log(e.target.attributes.href.value); //para verificar a que elemento damos click
+            const seccion = document.querySelector(e.target.attributes.href.value);
+
+            seccion.scrollIntoView({ 
+                //se le pasa el siguiente objeto de configuración
+                behavior:"smooth",
+            }); // con esto obtenemos el mismo efecto de salto directo
+        });
+    });
+}
 document.addEventListener("DOMContentLoaded", function () {
     crearGaleria();
 })
